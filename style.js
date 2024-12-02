60729611e4e8b55c13ac398ff9ce1bea41126ac9
Najuma Khatoon <najumakhatun1@gmail.com>
2024-12-02 09:07:09 +0545

    first commit

---
+++ style.js
@@ -0,0 +1,60 @@
+// for(i=0;i<20;i++)
+// {
+//     console.log("My name is nazma")
+// }
+
+// let sum = 0;
+
+// for(let i=0;i<=20;i++)
+// {
+//     Sum= sum + i;
+//     {
+//         console.log(Sum);
+//     }
+
+    
+// }
+
+
+// let countEven = 0; 
+
+// for(let i=1;i<=20;i++)
+//     if(i%2==0)
+// {
+//     countEven++;
+//     {
+//         console.log(countEven);
+//     }
+    
+// }
+
+// let sumEven = 0; 
+// for(let i=1;i<=20;i++){
+//     if(i%2==0){
+//         sumEven = sumEven + i;
+//     }
+
+// }
+// console.log(sumEven)
+    
+// let sumodd = 0; 
+// for(let i=1;i<=100;i++){
+//     if(i%2==1){
+//         sumodd = sumodd + i;
+//     }
+
+// }
+// console.log(sumodd)
+    
+// for(let i=1;i<=10;i++)
+//  {
+//     console.log(`5*${i}=${5*i}`)
+
+// }
+
+for(let i=1;i<=10;i++)
+    {
+       console.log(`19*${i}=${19*i}`)
+   
+   }
+    
